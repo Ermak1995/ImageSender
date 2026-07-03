4315d8029a31c8e9c5acad9cd7b8ed19d4b32933
ermak <ermakovila640@gmail.com>
2026-07-04 00:51:58 +0300

    add_s3_methods

--- s3_client.py
+++ s3_client.py
@@ -3,21 +3,19 @@ from io import BytesIO
 
 from minio import Minio
 
+
 class S3Client:
     def __init__(self, endpoint, access_key, secret_key, bucket_name):
         self.bucket = bucket_name
         self.client = Minio(endpoint=endpoint,
-                       access_key=access_key,
-                       secret_key=secret_key,
-                       secure=False)
+                            access_key=access_key,
+                            secret_key=secret_key,
+                            secure=False)
         if not self.client.bucket_exists(self.bucket):
             self.client.make_bucket(bucket_name=bucket_name)
             print(f"[S3] Создан бакет: {self.bucket}")
         else:
             print(f"[S3] Бакет уже существует: {self.bucket}")
-
-    def health_check(self):
-        return self.client.bucket_exists(self.bucket)
 
     def upload(self, filename: str, content_type: str, data: bytes) -> str:
         image_id = str(uuid.uuid4())  # генерируем уникальный ключ
@@ -32,3 +30,29 @@ class S3Client:
         )
 
         return image_id
+
+    def object_exists(self, image_id: str) -> bool:
+        try:
+            self.client.stat_object(self.bucket, image_id)
+            return True
+        except:
+            return False
+
+    def get_info(self, image_id: str) -> dict:
+        if not self.object_exists(image_id):
+            raise ValueError(f"Объект {image_id} не найден")
+        stat = self.client.stat_object(self.bucket, image_id)
+        filename = stat.metadata.get("x-amz-meta-filename", image_id)
+
+        return {
+            "image_id": image_id,
+            "filename": filename,
+            "size_bytes": stat.size,
+            "content_type": stat.content_type,
+            "created_at": stat.last_modified.isoformat(),
+        }
+
+    def delete(self, image_id: str):
+        if not self.object_exists(image_id):
+            raise ValueError(f"Объект {image_id} не найден")
+        self.client.remove_object(self.bucket, image_id)
--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from s3_client import S3Client
 
 s3 = S3Client("localhost:9000", "admin", "password12345", "images")
 image_id = s3.upload("test.jpg", "image/jpeg", b"fake data")
-# print(f"Загружено: {image_id}")
-# print(s3.get_info(image_id))
-# s3.delete(image_id)
-# print("Удалено")
+# image_id = '8537142a-bcc8-4084-b589-3dca6daf1a5f'
+print(f"Загружено: {image_id}")
+print(s3.get_info(image_id))
+s3.delete(image_id)
+print("Удалено")
